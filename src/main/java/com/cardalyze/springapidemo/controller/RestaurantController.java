a1ef067aeaeec48e109f6eb7d1b913ceec948960
Manish Verma <mverma@cardalyze.com>
2018-04-19 17:59:54 -0400

    Added other HTTP Methods

--- src/main/java/com/cardalyze/springapidemo/controller/RestaurantController.java
+++ src/main/java/com/cardalyze/springapidemo/controller/RestaurantController.java
@@ -3,7 +3,9 @@ package com.cardalyze.springapidemo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +23,21 @@ public class RestaurantController {
 	{
 		return restaurantService.getAllRestaurants();
 	}
-	
 	@PostMapping("/restaurants/")
 	public int addRestaurant(@RequestBody Restaurant restaurant)
 	{
 		if (restaurant == null)
 			throw new RestaurantServiceException("Request does not contain a valid Restaurant Object", null);
 		return restaurantService.addRestaurant(restaurant);
+	}
+	@GetMapping ("/restaurants/{id}")
+	public Restaurant getRestauntById(@PathVariable int id)
+	{
+		return restaurantService.getRestaurantById(id);
+	}
+	@DeleteMapping ("/restaurants/{id}")
+	public void removeRestaurant(@PathVariable int id)
+	{
+		restaurantService.deleteRestaurantById(id);
 	}
 }
